3d9909482dfbf90ddaf23d757c5265da2598a105
Julio Abalos <abalos.ju@gmail.com>
2021-09-06 08:19:20 -0300

    incorporaciond de NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+const NavBar = ()  => {
+
+    return <div>
+        <img src="../img/logo.png"/>
+        <ul>
+            <li>Comprar</li>
+            <li>Vender</li>
+            <li>Nosotros</li>
+        </ul>
+
+    </div>
+}
+
+export default NavBar
